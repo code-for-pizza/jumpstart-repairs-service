86575fa2a76a7bb135c2a31495b2d0774a549e67
Keith Francis Balde <keithfrancisb@gmail.com>
2022-06-07 23:47:33 +0000

    add another example Query field

--- src/schema/resolvers.ts
+++ src/schema/resolvers.ts
@@ -7,6 +7,9 @@ const resolvers = {
 
       return `Hello Keith`;
     },
+    huhu: (_: undefined, { huhu }: { huhu: string }) => {
+      return huhu;
+    },
   },
 };
 
--- src/schema/typeDefs.ts
+++ src/schema/typeDefs.ts
@@ -3,6 +3,7 @@ import { gql } from 'apollo-server';
 const typeDefs = gql`
   type Query {
     hello(name: String): String
+    haha(huhu: String): String!
   }
 `;
 
